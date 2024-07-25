f577ea4d603637ef54c5fac5ec152b80b472f000
laposhko <laposhko0@gmail.com>
2024-07-25 18:04:14 +0300

    fixed email validation err

--- src/validation/auth-validation.js
+++ src/validation/auth-validation.js
@@ -2,12 +2,12 @@ import Joi from 'joi';
 import { emailRegexp } from '../constants/contacts-constants.js';
 export const userSignupSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().min(5).max(20).pattern(emailRegexp).required(),
+  email: Joi.string().min(5).pattern(emailRegexp).required(),
   password: Joi.string().min(5).max(20).required(),
 });
 
 export const userSigninSchema = Joi.object({
-  email: Joi.string().min(5).max(20).pattern(emailRegexp).required(),
+  email: Joi.string().min(5).pattern(emailRegexp).required(),
   password: Joi.string().min(5).max(20).required(),
 });
 
